2db2e5e4744a89186ece0ebf989a8a44e8a500e8
lushengqin <2015941587@qq.com>
2021-03-09 23:39:17 +0800

    init目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+  <div>hi</div>
+  <Frank></Frank>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
---
+++ src/components/Frank.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        我是创建的第一个组件
+    </div>
+</template>
