19fb27a50fdc673c5eb48b15fd2113dcb4d74a4e
lushengqin <2015941587@qq.com>
2021-03-11 22:26:43 +0800

    topnav 组件完成

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-        <div class="topnav">
-            <div class="log"></div>
-            <div class="menu"></div>
-        </div>
-        <div class="content">
-            <aside>边栏</aside>
-            <main>内容</main>
-        </div>
+  <div>
+    <topnav />
+    <div class="content">
+      <aside>边栏</aside>
+      <main>内容</main>
     </div>
+  </div>
 </template>
+<script>
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: { Topnav },
+};
+</script>
