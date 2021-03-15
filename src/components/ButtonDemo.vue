0010dbed74796c7c56ecfbb63b9e4dab908ccf47
lushengqin <2015941587@qq.com>
2021-03-15 21:34:09 +0800

    button提交

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,21 @@
 <template>
   <div>我是buttondemo</div>
   <div>
-    <Button> 我是buttonDemo里面的按钮 </Button>
+    <Button @click="onClick" @mouseover="onClick" @focus="onClick">
+      我是buttonDemo里面的按钮
+    </Button>
   </div>
 </template>
 <script>
 import Button from "../lib/Button.vue";
 export default {
   components: { Button },
+  props: {},
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
 };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,23 @@
 <template>
-  <button>
-    <slot></slot>
-  </button>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot></slot>
+    </button>
+  </div>
 </template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  setup(props, context) {
+    //通过第二个参数得到
+    const { size, ...rest } = context.attrs;
+    return { size, rest };
+  },
+};
+</script>
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style>
