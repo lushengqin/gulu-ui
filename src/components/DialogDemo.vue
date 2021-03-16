9ce72b8627450b7678b59360167c16e9a27bbf3d
lushengqin <2015941587@qq.com>
2021-03-16 10:47:04 +0800

    让Dialog支持visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,11 @@
 <template>
   <div>DialogDemo</div>
+  <div>案例1</div>
+  <Dialog></Dialog>
 </template>
+<script>
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: { Dialog },
+};
+</script>
