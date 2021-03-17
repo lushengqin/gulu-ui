1e8d0b82fab290c1b1dd1aa1422ffe7cda346d19
lushengqin <2015941587@qq.com>
2021-03-17 14:47:52 +0800

    提交之 tabs切换+滑动的线

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,98 @@
 <template>
-  <div>Tabs</div>
+  <!-- <div> -->
+  <!-- <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+    <component v-for="(c, index) in defaults" :is="c" :key="index" /> -->
+  <!-- <component :is="defaults[1]" /> -->
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div
+        class="gulu-tabs-nav-item"
+        :class="{ selected: t === selected }"
+        @click="select(t)"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+      <div class="gulu-tabs-nav-indicator"></div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component
+        class="gulu-tabs-content-item"
+        :class="{ selected: c.props.title === selected }"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
+  setup(props, context) {
+    // console.log({ ...context.slots.default() }); log技巧
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须是 Tab");
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const select = (title: String) => {
+      context.emit("update:selected", title);
+    };
+    return { defaults, titles, select };
+  },
+};
+</script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+</style>
