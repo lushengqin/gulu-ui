55af4cca2ad774fa311eebcfa24c64b720b103d8
lushengqin <2015941587@qq.com>
2021-03-24 10:55:34 +0800

    报错<

--- rollup.config.js
+++ rollup.config.js
@@ -12,13 +12,13 @@ export default {
     globals: {
       vue: 'Vue'
     },
-    name: 'Gulu',
-    file: 'dist/lib/gulu.js',
+    name: 'xiaolu-ui',
+    file: 'dist/lib/xiaolu-ui.js',
     format: 'umd',
     plugins: [terser()]
   }, {
-    name: 'Gulu',
-    file: 'dist/lib/gulu.esm.js',
+    name: 'xiaolu-ui',
+    file: 'dist/lib/xiaolu-ui.esm.js',
     format: 'es',
     plugins: [terser()]
   }],
